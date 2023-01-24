b95c52b2487bdb04dd4c810eeeaaa3b2a03ad395
josecarlos47 <jcfj270504@gmail.com>
2023-01-24 09:43:04 +0100

    v0.3-resta

--- IncredibleCalculator.java
+++ IncredibleCalculator.java
@@ -22,6 +22,12 @@ public class IncredibleCalculator {
                     int n2=leerEnteroEntrada();
                     break;
             case 2: // Restar. Debes leer dos números enteros de entrada e imprimir su resta
+                    
+                     System.out.println("Diga el primer número")
+                    int n1=leerEnteroEntrada();
+                    System.out.println("Diga el segundo número");
+                    int n2=leerEnteroEntrada();
+                    
                     break;
             case 3: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su producto.
                     break;
